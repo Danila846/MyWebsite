df50a2b4300bc0ef937423bbfaa05452d55bf2fb
Dan846 <danilam846@gmail.com>
2021-10-04 03:32:51 +0700

    finish menг

--- js/script-menu.js
+++ js/script-menu.js
@@ -2,10 +2,16 @@ const burger = document.querySelector('.header__burger');
 const menuBurger = document.querySelector('.header__list');
 const header = document.querySelector('.header');
 
+const liEls = document.querySelectorAll('.header__list li');
+
 burger.addEventListener('click', () => {
 	burger.classList.toggle('active__burger');
 	menuBurger.classList.toggle('active__burger');
 	header.classList.toggle('active__burger');
 	document.body.classList.toggle('lock');
+	liEls.forEach(liEl => {
+		liEl.classList.toggle('fade')
+	})
 })
+
 
